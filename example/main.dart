dc96d0ff9d16c17f7122a2e74dc5aaa45912771c
kuan <yuankuan@gmail.com>
2019-06-24 21:41:17 +0800

    tried out all auth flow and login-required. All good

--- example/main.dart
+++ example/main.dart
@@ -3,20 +3,39 @@ import 'dart:html';
 import 'package:keycloak_dart/keycloak.dart';
 
 void main() async {
+  final loginButton = querySelector('#loginButton') as ButtonElement;
   final keycloak = KeycloakInstance();
-  await keycloak.init(KeycloakInitOptions());
-
-  var loginButton = querySelector('#loginButton') as ButtonElement;
-  loginButton.onClick.listen((event) async {
-    await keycloak.login();
-  });
-
-  String currentSituation;
-  if (keycloak.authenticated) {
-    currentSituation = 'Authenticated. You can only logout now.';
-  } else {
-    currentSituation = 'Please log in. ${keycloak.clientId}';
+  bool initSuccess = false;
+  try {
+    initSuccess = await keycloak.init(KeycloakInitOptions());
+  } on KeycloakError catch (e) {
+    print('error init: ${e.error}');
   }
 
-  querySelector('#output').text = currentSituation;
+  String currentSituation;
+  if (initSuccess) {
+    currentSituation = '''
+    <h3>Authenticated!</h3>
+    <strong>flow:</strong> ${keycloak.flow} <br>
+    <strong>token:</strong> ${keycloak.token} <br>
+    <strong>refreshToken:</strong> ${keycloak.refreshToken} <br>
+    ''';
+    loginButton.text = 'Logout';
+    loginButton.onClick.listen((event) async {
+      await keycloak.logout();
+    });
+  } else {
+    currentSituation = '''
+    <h3>Keycloak testing</h3>
+    <strong>token:</strong> ${keycloak.token} <br>
+    ''';
+    loginButton.text = 'Login';
+    loginButton.onClick.listen((event) async {
+      await keycloak.login();
+    });
+  }
+
+  querySelector('#output').innerHtml = currentSituation;
 }
+
+void _basicFlow() async {}
